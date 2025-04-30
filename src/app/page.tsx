4983090f13d615d7a6e1a30b49f72922c3132710
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-04-30 20:28:04 +0900

    style: 메인 페이지 스타일 구조 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import profileImg from "../../public/profileEX.png";
+
 export default function Home() {
   return (
     <div className="w-full flex mt-16 gap-30">
@@ -5,10 +8,10 @@ export default function Home() {
         <span className="text-[#FAA631] text-2xl">Posts</span>
         <div>
           <div className="py-8 border-b-1 border-[#D5D9E3]">
-            <div className="font-extrabold">
+            <div className="font-extrabold text-2xl">
               제목입니다제목입니다제목입니다제목입니다
             </div>
-            <div className="my-6">
+            <div className="my-6 text-[#B5BBC7]">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
               aperiam animi libero quae sint nobis molestiae suscipit
               perferendis facere quia! Vel obcaecati culpa ex libero tempore
@@ -18,12 +21,159 @@ export default function Home() {
               obcaecati culpa ex libero tempore consequuntur sapiente incidunt
               sint!
             </div>
-            <div>사용자사진 / 닉네임 / 날짜 / 태그</div>
+            <div className="flex gap-4 text-[14px] items-center h-12">
+              <div className="flex border-r-1 border-[#B5BBC7] items-center h-5 pr-1.5">
+                <Image
+                  src={profileImg}
+                  alt="사용사 이미지"
+                  width={35}
+                  height={35}
+                  className="m-2"
+                />
+                <span className="m-2">GyeongHwan Lee</span>
+              </div>
+              <div>2025. 04. 28</div>
+              <div className="flex gap-2">
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="py-8 border-b-1 border-[#D5D9E3]">
+            <div className="font-extrabold text-2xl">
+              제목입니다제목입니다제목입니다제목입니다
+            </div>
+            <div className="my-6 text-[#B5BBC7]">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+              aperiam animi libero quae sint nobis molestiae suscipit
+              perferendis facere quia! Vel obcaecati culpa ex libero tempore
+              consequuntur sapiente incidunt sint! Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Minima aperiam animi libero quae
+              sint nobis molestiae suscipit perferendis facere quia! Vel
+              obcaecati culpa ex libero tempore consequuntur sapiente incidunt
+              sint!
+            </div>
+            <div className="flex gap-4 text-[14px] items-center h-12">
+              <div className="flex border-r-1 border-[#B5BBC7] items-center h-5 pr-1.5">
+                <Image
+                  src={profileImg}
+                  alt="사용사 이미지"
+                  width={35}
+                  height={35}
+                  className="m-2"
+                />
+                <span className="m-2">GyeongHwan Lee</span>
+              </div>
+              <div>2025. 04. 28</div>
+              <div className="flex gap-2">
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+                <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl">
+                  #java
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex-1 border-2 border-[#FAA631]">
+      <div className="flex-1 px-10">
         <span className="text-[#FAA631] text-2xl">Tags</span>
+        <div className="flex gap-2 flex-col mt-7">
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #java
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajavajava
+          </div>
+          <div className="bg-[#F2F4F6] px-2 py-1.5 rounded-2xl w-fit">
+            #javajava
+          </div>
+        </div>
       </div>
     </div>
   );
