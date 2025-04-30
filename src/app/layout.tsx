9295598067beb91fb5c6a5dd8e5ded36ae4f89d6
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-04-30 14:41:43 +0900

    style: 게시글 목록 형식 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="px-[42px]">
+      <body className="mx-[48px]">
         <BlogHeader />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
 export default function Home() {
-  return <div className="bg-green-900">mainPage</div>;
+  return (
+    <div className="w-full flex mt-16 gap-30">
+      <div className="flex-3">
+        <span className="text-[#FAA631] text-2xl">Posts</span>
+        <div>
+          <div className="py-8 border-b-1 border-[#D5D9E3]">
+            <div className="font-extrabold">
+              제목입니다제목입니다제목입니다제목입니다
+            </div>
+            <div className="my-6">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+              aperiam animi libero quae sint nobis molestiae suscipit
+              perferendis facere quia! Vel obcaecati culpa ex libero tempore
+              consequuntur sapiente incidunt sint! Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Minima aperiam animi libero quae
+              sint nobis molestiae suscipit perferendis facere quia! Vel
+              obcaecati culpa ex libero tempore consequuntur sapiente incidunt
+              sint!
+            </div>
+            <div>사용자사진 / 닉네임 / 날짜 / 태그</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 border-2 border-[#FAA631]">
+        <span className="text-[#FAA631] text-2xl">Tags</span>
+      </div>
+    </div>
+  );
 }
