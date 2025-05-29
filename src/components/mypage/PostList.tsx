7682dc3cec488367160b294fe5ca58158a983729
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-05-29 21:57:36 +0900

    style: 마이페이지 작성 글 스타일링

--- src/components/mypage/PostList.tsx
+++ src/components/mypage/PostList.tsx
@@ -4,7 +4,7 @@ export default function PostList({ posts }: { posts: any[]; }) {
   return (
     <div className="flex flex-col gap-8 mt-8">
       {posts.map((post) => (
-        <PostCard key={post.id} post={post} highlighted={post.highlighted} />
+        <PostCard key={post.id} post={post} />
       ))}
     </div>
   );
