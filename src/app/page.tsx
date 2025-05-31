046c1737d4fa184e1511b7fea4a7812ba12edb69
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-05-31 21:41:56 +0900

    style: 로고 이미지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import mainLogo from "../../public/logo2.png";
+import mainLogo from "../../public/logo2.svg";
 import PostList from "../components/mypage/PostList";
 import TagBadge from "../components/TagBadge";
 
