865f6f3b5830d8ba57bb7c0df1ad5eae208b3ba0
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-06-10 01:17:07 +0900

    refactor: 로그인 페이지 태그 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,17 +4,23 @@ import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-20">
-      <Link href="/">
-        <Image
-          src="/loginPageImg.svg"
-          alt="메인페이지 로고"
-          width={320}
-          height={230}
-          className=""
-        />
-      </Link>
-      <GithubLoginButton />
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen gap-20">
+      <header>
+        <Link href="/" aria-label="메인페이지로 이동">
+          <Image
+            src="/loginPageImg.svg"
+            alt="Gamza Tech Blog 로고"
+            width={320}
+            height={230}
+            priority
+          />
+        </Link>
+      </header>
+
+      <section aria-label="로그인">
+        <h1 className="sr-only">로그인</h1>
+        <GithubLoginButton />
+      </section>
+    </main>
   );
 }
