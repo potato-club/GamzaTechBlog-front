6a9ab89586a465556d7ce64d0b922da84b72732a
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-06-12 17:23:52 +0900

    feat: 메인페이지 ISR 적용

--- src/components/MainPageContent.tsx
+++ src/components/MainPageContent.tsx
@@ -7,45 +7,8 @@ import Sidebar from "./Sidebar";
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 
-export default async function MainPageContent() { // 실제 페이지 내용을 렌더링하는 컴포넌트를 분리
-  // const tags = [
-  //   "# java",
-  //   "# javascript",
-  //   "# react",
-  //   "# nextjs",
-  //   "# javajavajavajava",
-  //   "# spring",
-  //   "# typescript",
-  //   "# nodejs",
-  // ];
-
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     title: "제목입니다제목입니다제목입니다제목입니다제목입니다제목입니다제목입니다제목입니다",
-  //     summary:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aperiam animi libero quae sint nobis molestiae suscipit perferendis facere quia! Vel obcaecati culpa ex libero tempore consequuntur sapiente incidunt sint!",
-  //     author: "GyeongHwan Lee",
-  //     date: "2025. 04. 28",
-  //     tags: ["# java", "# spring", "# backend"],
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Next.js로 무한스크롤 구현하기",
-  //     summary:
-  //       "Next.js에서 Intersection Observer API를 사용해 무한스크롤을 구현하는 방법을 정리합니다.",
-  //     author: "Jinwoo Park",
-  //     date: "2025. 04. 27",
-  //     tags: ["# nextjs", "# react", "# frontend"],
-  //   },
-  // ];
-  // postService 에서 posts를 가져오는 로직을 여기에 추가할 수 있습니다.
-
-  // 예시로 posts를 하드코딩했습니다. 실제로는 API 호출 등을 통해 데이터를 가져와야 합니다.
-  // const tags = await postService.getTags(); // 태그 목록을 가져오는 서비스
-
-  await delay(2000); // 2000ms = 2초
-
+export default async function MainPageContent() {
+  // await delay(2000); // 2000ms = 2초
 
   const postResponse = await postService.getPosts({
     sort: [
