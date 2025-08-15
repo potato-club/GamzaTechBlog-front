776bd78656f76092a552af0358780219001e4260
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-08-16 05:20:05 +0900

    fix: LogoSection 내 불필요한 router import 제거

--- src/components/shared/layout/LogoSection.tsx
+++ src/components/shared/layout/LogoSection.tsx
@@ -3,7 +3,6 @@
 import { BLOG_DESCRIPTIONS } from "@/constants/textConstants";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import router from "next/router";
 import { useEffect, useState } from "react";
 
 /**
