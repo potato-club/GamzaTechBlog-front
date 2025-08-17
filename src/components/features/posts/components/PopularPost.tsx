3962f20406f11d3fecabc1a1032cefa161ad9005
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-08-18 02:55:02 +0900

    chore: 코드 복원

--- src/components/features/posts/components/PopularPost.tsx
+++ src/components/features/posts/components/PopularPost.tsx
@@ -11,7 +11,7 @@ interface PopularPostProps {
 export default function PopularPost({ postId, title, author, profileImage }: PopularPostProps) {
   return (
     <Link href={`/posts/${postId}`}>
-      <article className="-m-2 mb-7 cursor-pointer rounded-md border border-transparent p-2 transition-colors hover:border-[#FAA631]">
+      <article className="-m-2 mb-7 cursor-pointer rounded-md border border-transparent p-2 transition-colors hover:text-[#FAA631]">
         <h4 className="font-medium">{title}</h4>
         <div className="mt-4 flex items-center gap-1 text-[12px] text-[#798191]">
           {profileImage ? (
