19d9f223d4b2a26ed9eb31fb0bdabc4ae446c8a5
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-11-06 23:57:21 +0900

    fix: Toast UI Editor CSS 동적 import 타입 선언 추가
    - Vercel 빌드 에러 해결을 위한 타입 선언 파일 추가
    - @toast-ui/editor/dist/toastui-editor.css 모듈 선언
    - TypeScript 컴파일 에러 수정

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,7 @@
 // 글로벌 타입 선언 파일
 // 필요한 경우 여기에 타입 선언 추가
 
+// Toast UI Editor CSS 동적 import를 위한 타입 선언
+declare module "@toast-ui/editor/dist/toastui-editor.css";
+
 export {};
---
+++ src/types/toastui.d.ts
@@ -0,0 +1,2 @@
+// Toast UI Editor CSS 타입 선언
+declare module "@toast-ui/editor/dist/toastui-editor.css";
